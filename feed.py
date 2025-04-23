ee68368d7c766e4fa7cce525748d2ab5944fe3d3
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2025-04-23 09:58:23 -0300

    Feed SharePoint data

---
+++ feed.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from src.graph import get_sharepoint_files
+from src.auth import get_confidential_token
+from src.files import write_docs_locally
+from src.docling_loader import load_documents_with_docling
+from src.database import index_in_milvus
+
+load_dotenv()
+
+if __name__ == "__main__":
+    # 1. Get access token to Graph API
+    token = get_confidential_token()
+
+    # 2. Retrieve documents from SharePoint
+    files = get_sharepoint_files(token)
+
+    # 3. Write files locally
+    document_paths = write_docs_locally(files)
+
+    # 4. Load Docling documents
+    docling_docs = load_documents_with_docling(document_paths)
+
+    # 5. Index in Milvus vector database
+    index_in_milvus(docling_docs)
