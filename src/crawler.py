53113185f48a5ea4abd65871559e5170bfd4ba98
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2025-04-24 19:25:33 -0300

    crawl_url method

---
+++ src/crawler.py
@@ -0,0 +1,31 @@
+from firecrawl import FirecrawlApp, ScrapeOptions
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+
+def crawl_url(url: str):
+    app = FirecrawlApp(api_key=config['FIRECRAWL_API_KEY'])
+
+    # Crawl a website:
+    crawled_results = app.crawl_url(
+        url,
+        limit=3,
+        scrape_options=ScrapeOptions(formats=['markdown']),
+        poll_interval=30
+    )
+
+    if crawled_results.status == "completed":
+        for doc in crawled_results.data:
+            if "markdown" in doc:
+                with open('files/crawling_results.txt', "wb") as f:
+                    f.write(doc.markdown)
+
+    # for i, crawled in enumerate(crawled_results):
+    #    print(f'Section {i}')
+    #    print(crawled)
+
+
+website = "https://www.csn.com.br/"
+
+crawl_url(website)
